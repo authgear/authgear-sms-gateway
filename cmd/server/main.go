475d43c2ccb57c0db2bfb53b1f79d1cf00c55168
Ken Chan <chankiyu22@gmail.com>
2024-09-09 16:07:27 +0800

    Load dotenv

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"errors"
+	"log"
 	"net/http"
 	"time"
+
+	"github.com/joho/godotenv"
 
 	"github.com/authgear/authgear-sms-gateway/pkg/handler"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil && !errors.Is(err, os.ErrNotExist) {
+		log.Printf("failed to load .env file: %s", err)
+	}
 	cfg, err := LoadConfigFromEnv()
 	if err != nil {
 		panic(err)
